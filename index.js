664f1732d54e6ad3f02e9ad343991e8fc6c34cac
Moses Oladimeji Oyelade <moses.oyelade@student.moringaschool.com>
2024-03-25 20:39:25 +0100

    Update index.js
    hamburger icon function update

--- index.js
+++ index.js
@@ -12,40 +12,10 @@ hamburger.addEventListener("click", () =>{
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () =>{
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
-    //const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-
-
-hamburger.addEventListener("click", () =>{
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
-
-// function to remove menu dropdown 
-
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () =>{
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
 
 }))
 
-// function to animate the project section
 
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
-        }
-
-    });
-});
-
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));
-}))
 
 // function to animate the project section
 
